5aab722b5f2ae46c121a2d2be6187dbc648d5996
Yangel Kirill <34452390+GluKhovKirill@users.noreply.github.com>
2025-11-24 19:04:10 +0300

    Initial

---
+++ src/car.py
@@ -0,0 +1,22 @@
+class Car:
+    def __init__(self, model: str, fuel_capacity: float) -> None:
+        self._model = model
+        self._max_fuel_capacity: float = fuel_capacity
+        self._fuel_in_tank: float = 0
+
+    def get_current_fuel_level(self) -> float:
+        return self._fuel_in_tank
+
+    def refuel_car(self, fuel_quantity: float):
+        if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
+            raise Exception('Вы пытаетесь залить слишком много бензина!')
+        self._fuel_in_tank += fuel_quantity
+
+    def drive(self, distance_km: float):
+        # Считаем, что расход 8 литров на 100 км
+        fuel_burned: int = 8 * (distance_km / 100)
+        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
+        # if self._fuel_in_tank < fuel_burned:
+        #     raise Exception('Не доедем жеж...')
+        self._fuel_in_tank -= fuel_burned
+        return self.get_current_fuel_level()
