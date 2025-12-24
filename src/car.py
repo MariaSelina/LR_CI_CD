f051dbcd770493edb59eaac393cdee461f363566
MariaSelina <mariaselina2006@gmail.com>
2025-12-24 04:01:14 +0300

    Update car.py

--- src/car.py
+++ src/car.py
@@ -1,22 +1,42 @@
-class Car:
-    def __init__(self, model: str, fuel_capacity: float) -> None:
-        self._model = model
-        self._max_fuel_capacity: float = fuel_capacity
-        self._fuel_in_tank: float = 0
+from __future__ import annotations
 
-    def get_current_fuel_level(self) -> float:
+from typing import Self
+
+
+class FuelOverflowError(Exception):
+    MESSAGE = "Вы пытаетесь залить слишком много бензина!"
+
+    def __init__(self: Self) -> None:
+        super().__init__(self.MESSAGE)
+
+
+class InsufficientFuelError(Exception):
+    MESSAGE = "Не доедем жеж..."
+
+    def __init__(self: Self) -> None:
+        super().__init__(self.MESSAGE)
+
+
+class Car:
+    def __init__(self: Self, model: str, fuel_capacity: float) -> None:
+        self._model = model
+        self._max_fuel_capacity = float(fuel_capacity)
+        self._fuel_in_tank = 0.0
+
+    def get_current_fuel_level(self: Self) -> float:
         return self._fuel_in_tank
 
-    def refuel_car(self, fuel_quantity: float):
+    def refuel_car(self: Self, fuel_quantity: float) -> None:
         if self._max_fuel_capacity - self._fuel_in_tank < fuel_quantity:
-            raise Exception("Вы пытаетесь залить слишком много бензина!")
+            raise FuelOverflowError from None
         self._fuel_in_tank += fuel_quantity
 
-    def drive(self, distance_km: float):
+    def drive(self: Self, distance_km: float) -> float:
         # Считаем, что расход 8 литров на 100 км
-        fuel_burned: int = 8 * (distance_km / 100)
-        # TODO: Вася, не забудь расскомментировать! Клиенты могут застрять!!11
+        fuel_burned: float = 8.0 * (distance_km / 100.0)
+
         if self._fuel_in_tank < fuel_burned:
-            raise Exception("Не доедем жеж...")
+            raise InsufficientFuelError from None
+
         self._fuel_in_tank -= fuel_burned
         return self.get_current_fuel_level()
